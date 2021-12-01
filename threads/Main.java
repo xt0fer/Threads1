3ddec9e3604442bf59125dd614b66d5f48697338
Kristofer Younger <kris@zipcodewilmington.com>
2021-12-01 12:14:11 -0500

    checkpoint

--- threads/Main.java
+++ threads/Main.java
@@ -7,13 +7,13 @@ public class Main {
         System.out.println("Starting Runnable threads");
         t1.start();
         t2.start();
-        System.out.println("Runnable Threads has been started");
+        System.out.println("Runnable Threads have been started");
         Thread t3 = new MyThread("t3");
         Thread t4 = new MyThread("t4");
         System.out.println("Starting MyThreads");
         t3.start();
         t4.start();
-        System.out.println("MyThreads has been started");
+        System.out.println("MyThreads have been started");
         
     }
 }
